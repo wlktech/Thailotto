9aee06ef587f9724a4ef1e6bc4c6f47a0241a250
Wai Linn Kyaw <108474411+wlktech@users.noreply.github.com>
2023-12-31 16:47:35 +0630

    a

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,10 +27,21 @@ export default function HomePage() {
         <marquee behavior="" direction="" className='py-4 bg-secondary text-light rounded-2'>
           {data?.banner_text?.text}
         </marquee>
+        <marquee className='text-center'>
+          {data?.banners?.map((banner, index) => (
+            <img className='m-3 rounded shadow' src={banner.img_url} alt={`Banner ${index}`} width={300} key={index} />
+          ))}
+        </marquee>
       </div>
-      <div className='text-center'>
-        {data?.banners?.map((banner, index) => (
-          <img className='m-3 rounded shadow' src={banner.img_url} alt={`Banner ${index}`} width={300} key={index} />
+
+      <div className="container text-center">
+        {data?.game_links?.map((link,index)=>(
+            <div key={index} className='d-inline-flex'>
+              <a href={link.link} target="_blank" rel="noreferrer" className='text-decoration-none m-3'>
+                <img className='rounded' src={link.img_url} width={200} alt="" />  
+                <p className='mt-3'>{link.name}</p>
+              </a>
+            </div>
         ))}
       </div>
     </>
